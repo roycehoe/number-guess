48abf527621f5195133c1e08fb38f5e791298b69
roycehoe <royceinbox@hotmail.com>
2023-11-12 16:54:22 +0800

    feat: Write a for loop

--- main.c
+++ main.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+int getFahrenheit(int celsius) {
+	return (celsius * 9/5) + 32;
+}
+
+
+
+
+
 int main() {
-	printf("hello world\n");
+	int start = 0;
+	int end = 10;
+
+	for (int i=0; i < end; i++) {
+		printf("%d", start);
+		start += 1;
+	}
 	return 0;
 }
