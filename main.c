3d8fef898e17315a6f2e5171ddf0c11d9e7ff9c2
roycehoe <royceinbox@hotmail.com>
2023-11-12 18:44:12 +0800

    feat: Create game with hardcoded winning number

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 int banner() {
 	printf("------------------------\n");
@@ -6,10 +7,32 @@ int banner() {
 	printf("----GUESS THE NUMBER----\n");
 	printf("------------------------\n");
 	printf("------------------------\n");
+	return 0;
+}
+
+int instructions() {
+	printf("Pick a number between 1 and 10. Your objective is to guess the number I am thinking of!");
+	printf("Your number: ");
+	return 0;
 }
 
 
 int main() {
+	int pickedNumber;
+	bool isPlaying = true;
+
 	banner();
+	instructions();
+
+	while (isPlaying) {
+		scanf("%d", &pickedNumber);
+		if (pickedNumber != 5) {
+			printf("Oops, that wasn't the number I was thinking of. Please try again");
+		}
+		else {
+			printf("----YOU WON!----\n");
+			isPlaying = false;
+		}
+	}
 	return 0;
 }
